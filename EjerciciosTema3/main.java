42cc570221d49840ee07cdb771cef46887a3f769
Cristian Cevallos <81939696+kristiancdev@users.noreply.github.com>
2022-10-07 22:20:33 -0500

    Mejoras de formato.
    Se agrega texto a las impresiones para entender lo realizado

--- EjerciciosTema3/main.java
+++ EjerciciosTema3/main.java
@@ -7,12 +7,15 @@ Online Java is a quick and easy tool that helps you to build, compile, test your
 public class Main
 {
     public static void main(String[] args) {
-        System.out.println(sumaNumeros(2,3,5));
+        System.out.println("------------- Primera Parte -------------");
+        System.out.println("La suma es de los tres números es:" + sumaNumeros(2,3,5));
         
+        System.out.println("------------- Segunda Parte -------------");
         Coche coche = new Coche("Honda", 4);
-        System.out.println(coche.numeroPuertas);
+        
+        System.out.println("Numero de puertas: " + Integer.toString(coche.numeroPuertas));
         coche.agregarPuerta(1);
-        System.out.println(coche.numeroPuertas);
+        System.out.println("Numero de puertas: " + Integer.toString(coche.numeroPuertas));
     }
     
     static int sumaNumeros(int numero1, int numero2, int numero3){
