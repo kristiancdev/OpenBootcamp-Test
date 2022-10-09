df42a4ac8f2783e54284c4b0f05a2db9b3986491
Kristian <115022668+kristian-inn@users.noreply.github.com>
2022-10-09 07:59:12 -0500

    Ejercicio 2 C#
    Signed-off-by: Kristian <115022668+kristian-inn@users.noreply.github.com>

---
+++ Curso_CSharp/Ejercicio2/Program.cs
@@ -0,0 +1,46 @@
+﻿//Ejercicio1
+Console.WriteLine("Ingrese su nombre");
+string? nombre = Console.ReadLine();
+Console.WriteLine("Ingrese su apellido");
+string? apellido = Console.ReadLine();
+Console.WriteLine("Ingrese su edad, por favor ingrese en números");
+int? edad = Int32.Parse(Console.ReadLine());
+Console.WriteLine("¿Sabe programar?, Opciones de respuesta: Si/No");
+string? sabeProgramar = Console.ReadLine();
+
+bool esProgramador = (sabeProgramar == "Si" ? true : false);
+
+Console.WriteLine($"Hola {nombre} {apellido} tienes {edad} años de edad, ¿Sabes programar? {sabeProgramar}");
+
+
+//Ejercicio2
+int puertas = 3;
+int ruedas = 4;
+string marca = "Chevrolet Vitara";
+DateTime ITVVigente = DateTime.Now;
+double peso = 1.125;
+double largo = 3.642;
+string material = "Todoterreno";
+string color = "Blanco";
+
+Console.WriteLine("\n================================FICHA TÉCNICA =================================");
+Console.WriteLine($"Marca: {marca}\nPuertas: {puertas}\nRudas: {ruedas}\nPeso: {peso}\nLargo: {largo}\nMaterial: {material}\nColor: {color}\nITVVigente: {ITVVigente.ToString()}");
+
+
+Console.WriteLine("\n================================ OPERADORES =================================");
+
+if (edad >= 18)
+{
+    Console.WriteLine("Mayor de edad");
+}
+else
+{
+    Console.WriteLine("Menor de edad");
+}
+
+char caracter = 'a';
+bool resultado = Char.IsLetter(caracter);
+Console.WriteLine(resultado);
+
+if (edad >= 18 && resultado) Console.WriteLine("Operador AND");
+if (edad >= 18 || resultado) Console.WriteLine("Operador OR");
